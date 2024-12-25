42f8f074ccc011efdf02a9aac2db63190cb8497c
BengisuEksi <bengisueksi60@gmail.com>
2024-12-25 23:44:00 +0300

    RSS - News Headlines
    This repository contains a simple RSS reader project built with C# .NET . The purpose of this project is to demonstrate the usage of RSS feeds by fetching and displaying news headlines from three different websites.

---
+++ HeadlineNews/HeadlineNews/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace HeadlineNews
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.milliyet_bttn = new System.Windows.Forms.Button();
+            this.haberturk_bttn = new System.Windows.Forms.Button();
+            this.cumhuriyet_bttn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(11, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(288, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(325, 17);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(288, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(636, 17);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(288, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // milliyet_bttn
+            // 
+            this.milliyet_bttn.BackColor = System.Drawing.Color.Transparent;
+            this.milliyet_bttn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.milliyet_bttn.Location = new System.Drawing.Point(11, 73);
+            this.milliyet_bttn.Name = "milliyet_bttn";
+            this.milliyet_bttn.Size = new System.Drawing.Size(288, 35);
+            this.milliyet_bttn.TabIndex = 3;
+            this.milliyet_bttn.Text = "Milliyet";
+            this.milliyet_bttn.UseVisualStyleBackColor = false;
+            this.milliyet_bttn.Click += new System.EventHandler(this.milliyet_bttn_Click);
+            this.milliyet_bttn.MouseEnter += new System.EventHandler(this.milliyet_bttn_MouseEnter);
+            this.milliyet_bttn.MouseLeave += new System.EventHandler(this.milliyet_bttn_MouseLeave);
+            // 
+            // haberturk_bttn
+            // 
+            this.haberturk_bttn.BackColor = System.Drawing.Color.Transparent;
+            this.haberturk_bttn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.haberturk_bttn.Location = new System.Drawing.Point(325, 73);
+            this.haberturk_bttn.Name = "haberturk_bttn";
+            this.haberturk_bttn.Size = new System.Drawing.Size(288, 35);
+            this.haberturk_bttn.TabIndex = 4;
+            this.haberturk_bttn.Text = "HaberTürk";
+            this.haberturk_bttn.UseVisualStyleBackColor = false;
+            this.haberturk_bttn.Click += new System.EventHandler(this.haberturk_bttn_Click);
+            this.haberturk_bttn.MouseEnter += new System.EventHandler(this.haberturk_bttn_MouseEnter);
+            this.haberturk_bttn.MouseLeave += new System.EventHandler(this.haberturk_bttn_MouseLeave);
+            // 
+            // cumhuriyet_bttn
+            // 
+            this.cumhuriyet_bttn.BackColor = System.Drawing.Color.Transparent;
+            this.cumhuriyet_bttn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cumhuriyet_bttn.Location = new System.Drawing.Point(636, 73);
+            this.cumhuriyet_bttn.Name = "cumhuriyet_bttn";
+            this.cumhuriyet_bttn.Size = new System.Drawing.Size(288, 35);
+            this.cumhuriyet_bttn.TabIndex = 5;
+            this.cumhuriyet_bttn.Text = "Cumhuriyet";
+            this.cumhuriyet_bttn.UseVisualStyleBackColor = false;
+            this.cumhuriyet_bttn.Click += new System.EventHandler(this.cumhuriyet_bttn_Click);
+            this.cumhuriyet_bttn.MouseEnter += new System.EventHandler(this.cumhuriyet_bttn_MouseEnter);
+            this.cumhuriyet_bttn.MouseLeave += new System.EventHandler(this.cumhuriyet_bttn_MouseLeave);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Location = new System.Drawing.Point(11, 114);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(913, 403);
+            this.listBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(937, 534);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.cumhuriyet_bttn);
+            this.Controls.Add(this.haberturk_bttn);
+            this.Controls.Add(this.milliyet_bttn);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RSS";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button milliyet_bttn;
+        private System.Windows.Forms.Button haberturk_bttn;
+        private System.Windows.Forms.Button cumhuriyet_bttn;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ HeadlineNews/HeadlineNews/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace HeadlineNews
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void bring_news(string link)
+        {
+            listBox1.Items.Clear();
+            XmlTextReader xmlread = new XmlTextReader(link);
+            while (xmlread.Read())
+            {
+                if(xmlread.Name == "title")
+                {
+                    listBox1.Items.Add(xmlread.ReadString());
+                }
+            }
+        }
+        private void milliyet_bttn_Click(object sender, EventArgs e)
+        {
+            string link = "http://www.milliyet.com.tr/rss/rssNew/gundemRss.xml";
+            bring_news(link);
+        }
+
+        private void cumhuriyet_bttn_Click(object sender, EventArgs e)
+        {
+            string link = "http://www.cumhuriyet.com.tr/rss/son_dakika.xml";
+            bring_news(link);
+        }
+
+        private void haberturk_bttn_Click(object sender, EventArgs e)
+        {
+            string link = "https://www.haberturk.com/rss";
+            bring_news(link);
+        }
+
+        private void milliyet_bttn_MouseEnter(object sender, EventArgs e)
+        {
+            milliyet_bttn.BackColor = Color.SlateBlue;
+        }
+
+        private void milliyet_bttn_MouseLeave(object sender, EventArgs e)
+        {
+            milliyet_bttn.BackColor = Color.Transparent;
+        }
+
+        private void haberturk_bttn_MouseEnter(object sender, EventArgs e)
+        {
+            haberturk_bttn.BackColor = Color.SlateBlue;
+        }
+
+        private void haberturk_bttn_MouseLeave(object sender, EventArgs e)
+        {
+            haberturk_bttn.BackColor = Color.Transparent;
+        }
+
+        private void cumhuriyet_bttn_MouseEnter(object sender, EventArgs e)
+        {
+            cumhuriyet_bttn.BackColor = Color.SlateBlue;
+        }
+
+        private void cumhuriyet_bttn_MouseLeave(object sender, EventArgs e)
+        {
+            cumhuriyet_bttn.BackColor = Color.Transparent;
+        }
+    }
+}
